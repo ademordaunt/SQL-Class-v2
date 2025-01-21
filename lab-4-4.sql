07f1130ad00dd723d4efc603b9fca2ac7d674bed
Austin DeMordaunt <austin.demordaunt@kellogg.northwestern.edu>
2025-01-21 02:53:34 +0000

    HW Lab

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -1,7 +1,13 @@
 -- Who was the leading home run hitter for each team in 2019?
 -- NOTE: need more advanced SQL to answer this question without
 --       raising a warning: "Field of aggregated query neither grouped nor aggregated"
-
+SELECT teams.name AS team_name, players.first_name, players.last_name, MAX(stats.home_runs) AS home_runs
+FROM stats
+INNER JOIN players ON players.id = stats.player_id
+INNER JOIN teams ON teams.id = stats.team_id
+WHERE teams.year = 2019
+GROUP BY teams.name
+ORDER BY teams.name;
 -- Expected result:
 --
 -- +-------------------------------+------------+-------------+----------------------+
